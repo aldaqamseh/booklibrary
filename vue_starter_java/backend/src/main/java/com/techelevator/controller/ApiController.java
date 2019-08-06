1697e0c67db28a654286280472f39800245f1150
Katie <ktm2me@gmail.com>
2019-08-06 11:12:12 -0400

    Updated dependencies with new Spring MVC framework (5.1.5) KT

--- vue_starter_java/backend/src/main/java/com/techelevator/controller/ApiController.java
+++ vue_starter_java/backend/src/main/java/com/techelevator/controller/ApiController.java
@@ -4,17 +4,12 @@ import com.techelevator.authentication.UnauthorizedException;
 import com.techelevator.model.Book;
 import com.techelevator.model.BookDao;
 import java.util.List;
+import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
@@ -44,4 +39,5 @@ public class ApiController {
     public List<Book> getBooks(){
      return bookDao.getAllBooks();
     }
+    
 }
