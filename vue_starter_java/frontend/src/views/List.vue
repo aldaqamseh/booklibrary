078eb1f2de1d77eb6ab75eb329816def859524e1
Katie <ktm2me@gmail.com>
2019-08-09 14:59:43 -0400

    Added list view

--- vue_starter_java/frontend/src/views/List.vue
+++ vue_starter_java/frontend/src/views/List.vue
@@ -1,5 +1,17 @@
 <template>
-    
+    <div class="reading-list">
+		<h1>Your Reading List</h1>
+			<div
+				v-for="book in books"
+				:key="book.id">
+
+               <h1>{{book.title}}</h1>
+               <h3> {{book.author}}</h3>
+               <p>{{book.description}}</p>
+
+			</div>
+		
+	</div>
 </template>
 
 <script>
@@ -10,7 +22,19 @@ export default {
                 "http://localhost:8080/AuthenticationApplication/api/reading-list/",
             books: [],
 }
+        },
+        methods:{
+        fetchAllBooks() {
+			fetch(this.API_URL)
+				.then(response => {
+					return response.json();
+				})
+				.then(books => {
+					this.books = books;
+					console.log(this.books);
+                });
         }
+		},
     }
 
 </script>
