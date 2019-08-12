77983133427dd907bd2095e7e5222afeab8a2b89
Mohammad Aldaqamseh <dagamsah@gmail.com>
2019-08-12 09:10:34 -0400

    ff

--- vue_starter_java/backend/src/test/java/com/techelevator/authentication/JdbcCommentsDaoInegrationTest.java
+++ vue_starter_java/backend/src/test/java/com/techelevator/authentication/JdbcCommentsDaoInegrationTest.java
@@ -1,7 +1,5 @@
 package com.techelevator.authentication;
 
-import static org.junit.Assert.assertEquals;
-
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,26 +8,44 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import com.techelevator.model.Book;
-import com.techelevator.model.BookDao;
 import com.techelevator.model.Comments;
 import com.techelevator.model.CommentsDao;
-import com.techelevator.model.JdbcBookDao;
 import com.techelevator.model.JdbcCommentsDao;
+import com.techelevator.model.JdbcPostDao;
+import com.techelevator.model.JdbcUserDao;
+import com.techelevator.model.Post;
+import com.techelevator.model.PostDao;
+import com.techelevator.model.User;
+import com.techelevator.model.UserDao;
 
 public class JdbcCommentsDaoInegrationTest extends DAOIntegrationTest {
 
-	private CommentsDao dao = new JdbcCommentsDao(getDataSource());
+	private CommentsDao daoComment = new JdbcCommentsDao(getDataSource());
+	private PostDao daoPost = new JdbcPostDao(getDataSource());
+	
+	private UserDao daouser = new JdbcUserDao(getDataSource(), null);
 	
 	private JdbcTemplate template;
 	private Comments testComment1;
 	private Comments testComment2;
 	private Comments testComment3;
+	private User user;
+	
+	private Post testPost;
 	@Before
 	public void setup() {
 		
-		dao = new JdbcCommentsDao(getDataSource());
+		daoComment = new JdbcCommentsDao(getDataSource());
 		template = new JdbcTemplate(getDataSource());
+		
+		testPost = new Post();
+		testPost.setBody("test body");
+		testPost.setId(2);
+		testPost.setTitle("test title");
+		testPost.setUserId(1);
+		testPost.setDatePosted(new Date(1989-8-06));
+		
+		daoPost.save(testPost);
 		
 		testComment1 = new Comments();
 		testComment2 = new Comments();
@@ -37,54 +53,20 @@ public class JdbcCommentsDaoInegrationTest extends DAOIntegrationTest {
 		
 		testComment1.setBody("test comments1 body");
 		testComment1.setDatePosted(new Date(1989-8-06));
-		testComment1.setUserId((long) 15);
-		testComment1.setPostId((long) 14);
+		testComment1.setUserId(1);
+		testComment1.setPostId(2);
 		
 		testComment2.setBody("test comments2 body");
 		testComment2.setDatePosted(new Date(1989-8-06));
-		testComment2.setUserId((long) 16);
+		testComment2.setUserId(1);
 	}
 	@Test
-	public void assertBooksAreEqual() {
+	public void assertCommentsAreEqual() {
 		List<Comments> getList1 = new ArrayList<>();
 		List<Comments> getList2 = new ArrayList<>();
-		//getList2.addAll(dao.getAllCommentsByPostId((long) 1));
-		//dao.save(testComment2);
-		dao.save(testComment1);
+	
+		daoComment.save(testComment1);
 	}
 }
 
 
-/*
-	
-	
-	@Test
-	public void assertBooksAreEqual() {
-		List<Book> getList1 = new ArrayList<>();
-		List<Book> getList2 = new ArrayList<>();
-		
-		getList2.addAll(dao.getAllBooks());
-		
-		
-		dao.save(testBook2);
-		dao.save(testBook1);
-		getList1.addAll(dao.getAllBooks());
-
-		testBook3 = getList1.get(getList1.size()-1);
-		
-		assertEquals(getList1.size(), getList2.size()+2);
-		assertEquals(testBook3.getAuthor(), testBook1.getAuthor());
-		assertEquals(testBook3.getTitle(), testBook1.getTitle());
-		assertEquals(testBook3.getGenre(), testBook1.getGenre());
-		assertEquals(testBook3.getDescription(), testBook1.getDescription());
-		assertEquals(testBook3.getPublishDate().toString(), testBook1.getPublishDate().toString());
-		assertEquals(testBook3.getDateAdded().toString(), testBook1.getDateAdded().toString());
-		assertEquals(testBook3.getIsbn(), testBook1.getIsbn());
-		assertEquals(testBook3.getImgUrl(), testBook1.getImgUrl());
-		
-		
-		
-
-	}
-}
-*/
