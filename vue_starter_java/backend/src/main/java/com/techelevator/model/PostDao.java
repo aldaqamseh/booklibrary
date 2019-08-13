2413d7642ecfb92d0b0f0acc9107ea6dae03c0ea
Katie <ktm2me@gmail.com>
2019-08-13 13:02:52 -0400

    Added get post by ID

--- vue_starter_java/backend/src/main/java/com/techelevator/model/PostDao.java
+++ vue_starter_java/backend/src/main/java/com/techelevator/model/PostDao.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public interface PostDao {
 	
- List<Post> getAllPostsByPostId(Long postId);
+ List<Post> getAllPostsByPostId(int postId);
  List<Post> getAllPosts();
 	 
 	 void save(Post savePost);
