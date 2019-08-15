5b49c6c451a4965891a2dadbcc49518843c0f5db
Luke Jarrett <lukej_93@yahoo.com>
2019-08-15 13:51:43 -0400

    Seeded Comments to all posts

--- vue_starter_java/backend/database/schema.sql
+++ vue_starter_java/backend/database/schema.sql
@@ -190,12 +190,57 @@ VALUES
 INSERT INTO forum_posts
   (user_username, title, body)
 VALUES
-  ('joe123', 'Loved "It"', 'The book IT, by Stephen King was a great coming of age tale about a lonely clown searcing for love in all the wrong places. Why were those children constantly running away? All he wanted was a friend. I think we''ve all been in that place friends, let''s not judge.'),
   ('luke123', 'Do you think Tolkien knew how popular the word ''Hobbit'' would become?', 'I mean think about it, the man wrote these books before the cell phone was even an idea in someones head, and now the word Hobbit is almost recognized everywhere.. I feel like that''s pretty incredible, what about you guys?'),
   ('kt123', 'I don''t like Harry Potter (Unpopular opinion by a landslide)', 'I think J.K Rowling was always a better storyteller than a writer. I read the Harry Potter series recently and I kept seeing so many plotholes. So, so many plotholes. Why would they guard the philosopher''s stone with riddles, not locks? Why didn''t Fred and George ever say something about Ron being with a man named Peter throughout book 1 and 2? The final book also wasn''t as good of a payoff either. The hallows just came out of nowhere, instead of focusing on the horcruxes like it should''ve (in my opinion). I still enjoyed reading it when I first read it as a non-enthusiast, but as the years went on I began to not like it as much. I think the way characters interact is really great, until the 5th book where Harry becomes an edgy angst lord. There are just so many things I realize isn''t all that great about this series. Please tell me what you think.'),
   ('moh123', 'East of Eden has broken my heart, put it back together, and it will still never be whole again', 'I finished ‘East of Eden’ about 4 months ago during a warm sunset in Miami, in a brick courtyard with a soft fountain burbling away and students bustling around. The breeze blew softly while I did everything I could not to tear up in public. This book has stuck with me and I still sit up at night thinking about it. I’ve read ‘The Awakening’, ‘Huckleberry Finn’, ‘Wuthering Heights’, ‘The Grapes of Wrath’, ‘The Great Gatsby’, and so many more well-written, poignant novels. But East of Eden won’t leave me. No book has come closer to expressing my outlook on life in general. How life is filled with boundless tragedy, how some of us can handle it and some cannot, how we all so desperately want to be good yet still do evil, and how the story of life is written in generations.'),
   ('luke123', 'I just read ''Player Piano'' and now I hate my job.', 'A week or so ago I finished read Kurt Vonnegut''s Player Piano. It was a fantastic read but has left me hating my job. I work at a factory and can''t help but be reminded of the way they held up engineers and management on a pedistal everytime I come into work. Has anyone else had similar experiences with fiction, or non-fiction books, changing their perspectives?'),
   ('kt123', 'Just completed reading my first book', 'I''ve been thinking to get into reading since a very long time but for some reason I couldn''t. I either didn''t have enough motivation or I wasn''t consistent enough. I''d start reading a book and forget about it the next day. But a friend suggested "the alchemist" and I was totally immersed in it. I completed it in 3 days (around 5 hours of reading) and I''m very proud of myself. I hope this doesn''t end here and I keep on exploring books.'),
-  ('joe123', '“The Left Hand of Darkness” by Ursula K. Le Guin should be the gold standard of science fiction', 'I see a lot of posts on this sub related to science fiction, and I don’t think this book gets enough credit. The intricacy of the plot, the depth of the characters, and the all-encompassing portrait of the foreign planet “Gethen”, complete with its own societal constructs, religion, and different nations, make this work a true MASTERPIECE of science fiction. I don’t want to take anything away from other great science fiction works like Dune or Ender’s Game or The Lord of the Rings, etc, etc, but TLHOD is not has a great plot, but it has great allegory; it really makes you think about how your life is shaped by things like societal norms, weather, “truth”, relationships, religion, and most importantly gender. The writing is so intentional with every sentence, and it only gets more complex with every re-read. Le Guin deserves every word of the praise that she gets, but the praise that she gets isn’t even half of what she deserves.');
+  ('joe123', '“The Left Hand of Darkness” by Ursula K. Le Guin should be the gold standard of science fiction', 'I see a lot of posts on this sub related to science fiction, and I don’t think this book gets enough credit. The intricacy of the plot, the depth of the characters, and the all-encompassing portrait of the foreign planet “Gethen”, complete with its own societal constructs, religion, and different nations, make this work a true MASTERPIECE of science fiction. I don’t want to take anything away from other great science fiction works like Dune or Ender’s Game or The Lord of the Rings, etc, etc, but TLHOD is not has a great plot, but it has great allegory; it really makes you think about how your life is shaped by things like societal norms, weather, “truth”, relationships, religion, and most importantly gender. The writing is so intentional with every sentence, and it only gets more complex with every re-read. Le Guin deserves every word of the praise that she gets, but the praise that she gets isn’t even half of what she deserves.'),
+  ('joe123', 'Loved "It"', 'The book IT, by Stephen King was a great coming of age tale about a lonely clown searcing for love in all the wrong places. Why were those children constantly running away? All he wanted was a friend. I think we''ve all been in that place, let''s not judge.');
+
+INSERT INTO forum_comments
+(post_id, user_username, body)
+VALUES
+(7, 'luke123', 'I totally agree, friendship isn''t easy to come by, especially in your 20''s, also the ''Loser Club'' is very derogatory.'),
+(7, 'kt123', 'Are you serious? Clowns are terrifying. I would run as fast as I could'),
+(7, 'moh123', 'THIS. Clown culture needs to end. Seriously.'),
+(7, 'joe123', 'I think you both are being very close minded and missing the point of my post.'),
+(7, 'kt123', 'And YOU are crazy if you think Clowns + Kids = Friendship.'),
+
+(1, 'luke123', 'Let me know what you think :)'),
+(1, 'kt123', 'Haha great point! I never thought about that.'),
+(1, 'moh123', 'He would be so proud. Hobbit should be added to the Dictionary!'),
+(1, 'joe123', 'I''ve never read the Hobbit but I agree'),
+(1, 'kt123', 'Honestly, not missing much. Downvote me all you want.'),
+
+(2, 'luke123', 'The series is fun and I loved it. But it is full of moments as a reader where you realize, “Hang on, if X exists in the magical world then why didn’t they use it back in book Y to solve problem Z?”'),
+(2, 'kt123', 'Yeah, it''s full to the brim with literal magic macguffins. You can''t expect a seamless plot when the basis of the story teaching children to use catastrophically powerful weapons that double as plot hole filler sticks as needed'),
+(2, 'moh123', 'It''s fine to not like things.'),
+(2, 'joe123', 'The realization that a fun children''s series, or series you can enjoy like a child, is not great literature can be traumatizing. Roll with it. Life will be full of experiences like this and would be a lot less interesting without them.'),
+(2, 'kt123', 'A part of my soul shattered when I tried to reread David Eddings in my thirties.'),
+
+(3, 'luke123', 'East of Eden is my favorite book, too. I’d like to read it again, and you just convinced me. Steinbeck is so brilliant with words. Cannery Row is another of his great classics. EoE stuck with me too, for a long, long time.'),
+(3, 'kt123', 'I''ve commented this before, but East of Eden was so good that when I first started it, I put it back down because I just wasn''t ready to have it end. I picked it up years later and finished it, then named one of my twins Cal. I love it so so much.'),
+(3, 'moh123', 'I’ve found that readers that focus mostly on the story and plot don’t enjoy EoE as much as people who are more imaginative and visual....the world building is beautiful...I could almost feel the dust in the Salinas valley and imagine myself there. For me it evoked intense feelings of melancholy and wanderlust'),
+(3, 'joe123', 'I didn''t like this book either. I actually thought the start was the best part, but got really tedious as it went on.'),
+(3, 'kt123', 'That''s funny, I always tell people you have to get through the first 150-200 pages of the novel. I disagree with the others, I think it does pick up a lot'),
+
+(4, 'kt123', 'Welcome to Vonnegut'),
+(4, 'luke123', 'Oh no what have I done.'),
+(4, 'moh123', 'Was about to say. That''s not a side effect, it''s a feature.'),
+(4, 'joe123', 'This ''Looking for Alaska'' passage still haunts me: "You spend your whole life stuck in the labyrinth, thinking about how you''ll escape one day, and how awesome it will be, and imagining that future keeps you going, but you never do it. You just use the future to escape the present."'),
+(4, 'kt123', 'Probably in my top 3 Vonnegut books. My father has worked in a factory for 15 years and said the exact same things!'),
+
+(5, 'luke123', 'Good for you! Taking that initial jump of getting into a book is the hardest part! It only gets better from here!'),
+(5, 'kt123', 'I hope it does...'),
+(5, 'moh123', 'Congrats mate! Reading is so rewarding and I am happy to hear you picked up the hobby. You''ve got so much content to discover!'),
+(5, 'joe123', 'Congrats, man! I am 23 and read my first book last year. I am on my 18th book now. I feel bad that I didn''t start reading earlier. But, better late than never I guess.'),
+(5, 'kt123', 'That was my first book too, an amazing book to into reading.'),
+
+(6, 'luke123', 'I''ve added the book to my ever growing list of things to read. Have to say though, you placing LotR in the Science Fiction category is bothering me more than it should.'),
+(6, 'kt123', 'Their flippant use of the Science Fiction label aside, I can definitely recommend the book too. It''s the first book I have read every day until finished in 3 or 4 years.'),
+(6, 'moh123', 'Everything by LeGuin is a treasure. I also especially enjoyed The Dispossessed, and The Lathe of Heaven. If you haven''t read those yet, you are missing out.'),
+(6, 'joe123', 'I totally agree. Love Le Guin. One of the greatest writers of fiction ever to live.'),
+(6, 'kt123', 'I always start to read and stop in the first chapter. One day I''ll be able to read it all.');
 
 COMMIT TRANSACTION;
